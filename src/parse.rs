3fe02ca8bb0aff8aedb0fc7167cd5e5629942223
Unpublished <Unpublished@users.noreply.github.com>
2024-03-28 20:55:15 +0100

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -7,7 +7,7 @@ use std::mem;
 use std::path::Path;
 use thiserror::Error;
 use utmp_raw::{utmp, x32::utmp as utmp32, x64::utmp as utmp64};
-use zerocopy::{FromBytes, LayoutVerified};
+use zerocopy::{FromBytes, Ref};
 
 #[doc(hidden)]
 pub struct UtmpParserImpl<R, T = utmp>(R, PhantomData<T>);
@@ -112,7 +112,7 @@ fn read_entry<R: Read, T: FromBytes>(
         }
     }
     Ok(Some(
-        LayoutVerified::<_, T>::new(buffer).unwrap().into_ref(),
+        Ref::<_, T>::new(buffer).unwrap().into_ref(),
     ))
 }
 
